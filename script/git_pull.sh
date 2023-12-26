95ca95d49daca6210038f065b0f03615edb8b179
seunghunkim90 <154571086+seunghunkim90@users.noreply.github.com>
2023-12-26 17:19:07 +0900

    Update git_pull.sh

--- script/git_pull.sh
+++ script/git_pull.sh
@@ -1,1 +1,1 @@
-cd ~/e-bond-matching-api; git checkout main; git pull; > /home/ec2-user/test/log.txt
+cd ~/e-bond-matching-api; git checkout main > /home/ec2-user/test/log1.txt; git pull > /home/ec2-user/test/log2.txt;
